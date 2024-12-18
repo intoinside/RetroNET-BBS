d98f1f7815aae28c9c8aeae1a855999c1f6df61b
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2024-12-18 22:53:17 +0100

    First version of petscii encoder

---
+++ RetroNet-BBS/Encoders/Petscii.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace RetroNet_BBS.Encoders
+{
+    public static class Petscii
+    {
+        public static byte[] FromAscii(string s)
+        {
+            s = s.Replace("<white>", new String((char)5, 1), true, null);
+            s = s.Replace("<red>", new String((char)28, 1), true, null);
+            s = s.Replace("<green>", new String((char)30, 1), true, null);
+            s = s.Replace("<blue>", new String((char)31, 1), true, null);
+            s = s.Replace("<orange>", new String((char)129, 1), true, null);
+            s = s.Replace("<black>", new String((char)144, 1), true, null);
+            s = s.Replace("<lightred>", new String((char)150, 1), true, null);
+            s = s.Replace("<lightgreen>", new String((char)153, 1), true, null);
+            s = s.Replace("<lightblue>", new String((char)154, 1), true, null);
+            s = s.Replace("<purple>", new String((char)156, 1), true, null);
+            s = s.Replace("<yellow>", new String((char)158, 1), true, null);
+            s = s.Replace("<cyan>", new String((char)159, 1), true, null);
+
+            var output = new byte[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                var charToConvert = (int)s[i];
+
+                if (charToConvert >= 65 && charToConvert <= 90)
+                {
+                    output[i] = (byte)(charToConvert + 32);
+                    continue;
+                }
+
+                if (charToConvert >= 97 && charToConvert <= 122)
+                { 
+                    output[i] = (byte)(charToConvert - 32);
+                    continue;
+                }
+
+                output[i] = (byte)charToConvert;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ RetroNet-BBS/Server/Server.cs
@@ -0,0 +1,89 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace RetroNet_BBS.Server
+{
+    public class Server
+    {
+        private TcpListener Listener;
+        private readonly string IpAddress;
+        private readonly int Port;
+
+        public Server(string host)
+        {
+            IpAddress = "192.168.1.2";
+            Port = 8502;
+        }
+
+        public async Task Start()
+        {
+            // Abort operation if server is already running
+            if (Listener != null)
+            {
+                throw new InvalidOperationException("Server is already running!");
+            }
+
+            IPAddress localAddr = IPAddress.Parse(IpAddress);
+            Listener = new TcpListener(localAddr, Port);
+            Listener.Start();
+
+            OnMessageReceived("Server started. Waiting for a connection...");
+
+            while (true)
+            {
+                // Handle new client connection
+                TcpClient client = await Listener.AcceptTcpClientAsync();
+                OnMessageReceived($"Connected! Client IP: {client.Client.RemoteEndPoint}");
+                _ = HandleClientAsync(client);
+            }
+        }
+
+        public void Stop()
+        {
+            Listener?.Stop();
+            Listener = null;
+        }
+
+        private async Task HandleClientAsync(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            StringBuilder messageBuilder = new StringBuilder();
+
+            // Receive data in a loop until the client disconnects
+            while (true)
+            {
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                if (bytesRead == 0)
+                {
+                    OnMessageReceived($"Client {client.Client.RemoteEndPoint} disconnected.");
+                    break; // Client disconnected
+                }
+
+                string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                messageBuilder.Append(data);
+
+                if (data.Length > 0)
+                {
+                    string receivedMessage = messageBuilder.ToString().TrimEnd();
+
+                    // Raise the MessageReceived event
+                    OnMessageReceived($"{client.Client.RemoteEndPoint}: {receivedMessage}");
+
+                    byte[] response = Encoders.Petscii.FromAscii("<yellow>Hello <green>world! <white>0<orange>1<red>2<green>3<lightgreen>4<blue>5<lightblue>6789");
+                    await stream.WriteAsync(response, 0, response.Length);
+
+                    messageBuilder.Clear();
+                }
+            }
+        }
+
+        protected virtual void OnMessageReceived(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
