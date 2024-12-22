85629ca6866806fd9a40e21061e52bb4f000cb38
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2024-12-22 15:42:00 +0100

    Renaming, with support for coded color and page footer

---
+++ RetroNet-BBS/Pages/Footer.cs
@@ -0,0 +1,45 @@
+﻿using RetroNET_BBS.Encoders;
+using System.ComponentModel;
+
+namespace RetroNET_BBS.Pages
+{
+    public class Footer
+    {
+        public static string ShowFooter(string navigationOptions, Colors? color = null)
+        {
+            var output = "\x13";
+            for (int i = 0; i < 24; i++)
+            {
+                output += "\x11";
+            }
+
+            if (color != null && Enum.IsDefined(typeof(Colors), color))
+            {
+                output += "<" + color.GetDescription() + ">";
+            }
+
+            output += navigationOptions;
+            return output;
+        }
+
+    }
+
+    public static class Extensions
+    {
+        static public string GetDescription(this Enum enumValue)
+        {
+            var field = enumValue.GetType().GetField(enumValue.ToString());
+            if (field == null)
+                return enumValue.ToString();
+
+            var attributes = field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+            {
+                return attribute.Description;
+            }
+
+            return enumValue.ToString();
+        }
+    }
+
+}
