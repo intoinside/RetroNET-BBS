e8e316c1dfd7f1fde6234f6297fca92d80a25448
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2024-12-22 15:59:03 +0100

    wip

--- RetroNet-BBS/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using RetroNET_BBS.Server;
-
-Console.WriteLine("Hello, World!");
-
-var svr = new Server("127.0.0.1");
-await svr.Start();
-Console.WriteLine("Goodbye, World!");
